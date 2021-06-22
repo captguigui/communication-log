37e8004be12894925460151c68e42b661b9d13ce
Guillermo Jose Frenero-Sanchez <guigui@MacBook-Pro.local>
2021-06-22 12:19:05 -0400

    Added new line for pilot

--- Ch15-Studio/Program.cs
+++ Ch15-Studio/Program.cs
@@ -7,6 +7,7 @@ namespace Ch15_Studio
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Pilot!");
         }
     }
 }
